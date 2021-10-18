3e29ddeaf7f644ebd03b34f7dae2546903c348fc
Jean Paul Delaye <92291163+jpdelaye@users.noreply.github.com>
2021-10-17 23:03:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
+/**
+ * rectanglesrandomfilled Class
+ * This class will output 3 sgv rectangles filled with a random color each one.
+ * @author Jean Paul Delaye 
+ */
+
 require_once('class.random-colors-3-rectangles.php');
 
-  $artwork = $artwork = new rectanglesrandomfilled( );
+$artwork = $artwork = new rectanglesrandomfilled( );
 
 
 ?>
